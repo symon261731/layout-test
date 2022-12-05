6e8ee1f76484c9174d63571e5ce619671fd4f290
Simon Mishchenko <symon261731@gmail.com>
2022-12-05 21:26:36 +0300

    добавлена логика для отлавливания места остановки скрола

--- client/src/hooks/useHorizontalHook.tsx
+++ client/src/hooks/useHorizontalHook.tsx
@@ -1,31 +1,10 @@
+
 import { useRef, useEffect } from "react";
 import useWindowDimensions  from './useWindowDimens';
 
 export function useHorizontalScroll() {
   const elRef = useRef<null | HTMLDivElement>(null); 
   const { width } = useWindowDimensions();
-  // useEffect(() => {
-  //   const el = elRef.current;
-  //   if (el) {
-  //     const onWheel = (e: any) => {
-  //       if (e.deltaY === 0) return;
-  //       if (
-  //         !(el.scrollLeft === 0 && e.deltaY < 0) &&
-  //         !(el.scrollWidth - el.clientWidth - Math.round(el.scrollLeft) === 0 && 
-  //             e.deltaY > 0)
-  //       ) {
-  //         e.preventDefault();
-  //       }
-  //       el.scrollTo({
-  //         left: el.scrollLeft + e.deltaY,
-  //         behavior: 'smooth'
-  //       });
-  //     };
-  //     el.addEventListener('wheel', onWheel);
-  //     return () => el.removeEventListener('wheel', onWheel);
-  //   }
-  // }, []);
-  // return elRef;
 
     useEffect(() => {
       const el = elRef.current;
@@ -38,7 +17,9 @@ export function useHorizontalScroll() {
         el.addEventListener("wheel", onWheel);
         return () => el.removeEventListener("wheel", onWheel);
       }
-    }, []);
+    }, [width]);
+    console.log('render');
+    
     return elRef;
   }
 
--- client/src/view/Main/Slider/Slider.tsx
+++ client/src/view/Main/Slider/Slider.tsx
@@ -1,21 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHorizontalScroll } from '../../../hooks/useHorizontalHook';
-import useWindowDimensions from '../../../hooks/useWindowDimens';
 import './Slider.scss';
 
 
 const Slider : React.FC = () => {
-    const { width } = useWindowDimensions();
     const scroolRef= useHorizontalScroll();
-
+    const [position, setPosition] = useState(0);
     
+    const handleScroll = () => {
+            const positionY = window.pageYOffset;
+            setPosition(positionY);
+        }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', handleScroll);
+        return ()=>{
+            window.removeEventListener('scroll', handleScroll);
+        }
+    },[])
+    useEffect(()=>{
+        if( position >= 1610 || position >=1560){
+            const elem = document.getElementsByClassName('main');
+            console.log(elem)
+        }
+    },[position])
+
+
     return (
         <section className="main__section slider">
-            <div className="slider">
-                <p>{width}</p>
-                <div className="slider__container">
+            {position}
+            <div  className="slider">                
+                <div  className="slider__container">
                     <h2 className="slider__title"> Lorem ipsum dolor sit amet </h2>
-                    <div ref={scroolRef} className="slider__photos">
+                    <div ref={scroolRef}  className="slider__photos">
                         <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash-1.webp" alt="one" className="slider__image" /></div>
                         <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash-2.webp" alt="two" className="slider__image" /></div>
                         <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 3.webp" alt="three" className="slider__image" /></div>
@@ -26,6 +43,18 @@ const Slider : React.FC = () => {
                         <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 8.webp" alt="eight" className="slider__image" /></div>
                         <div className="slider__picture"><img src="/slider/webp/pexels-ruby-ruby-14471061.webp" alt="nine" className="slider__image" /></div>
                         <div className="slider__picture"><img src="/slider/webp/pexels-eugenia-remark-14558307.webp" alt="ten" className="slider__image" /></div>
+
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash-1.webp" alt="one" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash-2.webp" alt="two" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 3.webp" alt="three" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 4.webp" alt="four" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 5.webp" alt="five" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 6.webp" alt="six" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 7.webp" alt="seven" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/andres-vera-CmmYT6Mm948-unsplash 8.webp" alt="eight" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/pexels-ruby-ruby-14471061.webp" alt="nine" className="slider__image" /></div>
+                        <div className="slider__picture"><img src="/slider/webp/pexels-eugenia-remark-14558307.webp" alt="ten" className="slider__image" /></div>
+
                     </div>
                 </div>    
             </div>
